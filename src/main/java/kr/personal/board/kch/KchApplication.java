3a1b9f70f7d005460d34c57a1629208982546a8c
kimchanghyeondev <kimchanghyeon1201@gmail.com>
2019-12-05 22:51:06 +0900

    프로젝트 생성

---
+++ src/main/java/kr/personal/board/kch/KchApplication.java
@@ -0,0 +1,13 @@
+package kr.personal.board.kch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/personal/board/kch/KchApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.personal.board.kch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
